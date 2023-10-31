f3229de7cc5d44b6b1adc6dcc7c22054f4db5b12
cdincer <dincercan@gmail.com>
2023-10-31 15:35:58 +0300

    Slimming Domain down for Docker adjustments

--- cmclean.Domain/Model/Contact.cs
+++ cmclean.Domain/Model/Contact.cs
@@ -3,15 +3,15 @@
 namespace cmclean.Domain.Model;
 public class  Contact: BaseEntity
 {
-    public string? Salutation { get; set; }
+    //public string? Salutation { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? DisplayName => DisplayName ?? $"{FirstName} {LastName}";
     public DateTime BirthDate { get; set; }
-    public string Email { get; set; }
-    public string? PhoneNumber { get; set; }
-    public DateTime CreationTimestamp { get; set; }
-    public DateTime LastchangeTimestamp { get; set; }
+    //public string Email { get; set; }
+    //public string? PhoneNumber { get; set; }
+    //public DateTime CreationTimestamp { get; set; }
+    //public DateTime LastchangeTimestamp { get; set; }
 
 
     public void UpdateContact(string firstName, string lastName, DateTime dateOfBirth)
