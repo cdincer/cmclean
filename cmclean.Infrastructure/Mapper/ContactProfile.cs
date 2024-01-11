16978514a0b5433fc04270cb017e93776e6b5e41
cdincer <dincercan@gmail.com>
2024-01-11 12:17:04 +0300

    ContactProfile code block format lining up

--- cmclean.Infrastructure/Mapper/ContactProfile.cs
+++ cmclean.Infrastructure/Mapper/ContactProfile.cs
@@ -18,60 +18,60 @@ public class ContactProfile : Profile
     public ContactProfile()
     {
         CreateMap<ContactProtoModel, GetAllContactsResponse>()
-            .ForMember(dest => dest.BirthDate, opt =>
-            opt.MapFrom(src => src.BirthDate.ToDateTime()))
-            .ForMember(dest => dest.CreationTimestamp, opt =>
-            opt.MapFrom(src => src.CreationTimestamp.ToDateTime()))
-            .ForMember(dest => dest.LastChangeTimeStamp, opt =>
-            opt.MapFrom(src => src.LastChangeTimeStamp.ToDateTime()));
+        .ForMember(dest => dest.BirthDate, opt =>
+        opt.MapFrom(src => src.BirthDate.ToDateTime()))
+        .ForMember(dest => dest.CreationTimestamp, opt =>
+        opt.MapFrom(src => src.CreationTimestamp.ToDateTime()))
+        .ForMember(dest => dest.LastChangeTimeStamp, opt =>
+        opt.MapFrom(src => src.LastChangeTimeStamp.ToDateTime()));
 
         CreateMap<ContactProtoModel, GetContactByFilterResponse>()
-            .ForMember(dest => dest.BirthDate, opt =>
-            opt.MapFrom(src => src.BirthDate.ToDateTime()))
-            .ForMember(dest => dest.CreationTimestamp, opt =>
-            opt.MapFrom(src => src.CreationTimestamp.ToDateTime()))
-            .ForMember(dest => dest.LastChangeTimeStamp, opt =>
-            opt.MapFrom(src => src.LastChangeTimeStamp.ToDateTime()));
+        .ForMember(dest => dest.BirthDate, opt =>
+        opt.MapFrom(src => src.BirthDate.ToDateTime()))
+        .ForMember(dest => dest.CreationTimestamp, opt =>
+        opt.MapFrom(src => src.CreationTimestamp.ToDateTime()))
+        .ForMember(dest => dest.LastChangeTimeStamp, opt =>
+        opt.MapFrom(src => src.LastChangeTimeStamp.ToDateTime()));
 
         CreateMap<ContactProtoModel, GetContactByIdResponse>()
-            .ForMember(dest => dest.BirthDate, opt =>
-            opt.MapFrom(src => src.BirthDate.ToDateTime()))
-            .ForMember(dest => dest.CreationTimestamp, opt =>
-            opt.MapFrom(src => src.CreationTimestamp.ToDateTime()))
-            .ForMember(dest => dest.LastChangeTimeStamp, opt =>
-            opt.MapFrom(src => src.LastChangeTimeStamp.ToDateTime()));
+        .ForMember(dest => dest.BirthDate, opt =>
+        opt.MapFrom(src => src.BirthDate.ToDateTime()))
+        .ForMember(dest => dest.CreationTimestamp, opt =>
+        opt.MapFrom(src => src.CreationTimestamp.ToDateTime()))
+        .ForMember(dest => dest.LastChangeTimeStamp, opt =>
+        opt.MapFrom(src => src.LastChangeTimeStamp.ToDateTime()));
 
         CreateMap<ContactProtoModel, CreateContactRequest>()
-            .ForMember(dest => dest.BirthDate, opt =>
-                opt.MapFrom(src => src.BirthDate.ToDateTime()));
+        .ForMember(dest => dest.BirthDate, opt =>
+        opt.MapFrom(src => src.BirthDate.ToDateTime()));
 
         CreateMap<GetContactByFilterQuery, GetContactByFilterProtoRequest>()
         .ForMember(dest => dest.BirthDate, opt =>
         opt.MapFrom(src => Timestamp.FromDateTime(src.BirthDate.ToUniversalTime()))).ReverseMap();
 
-        CreateMap<CreateContactProtoResponse, CreateContactResponse > ().
-                        ForPath(dest => dest.Id, opt => opt.MapFrom(src => src.Data.Id)).
-                        ForPath(dest => dest.Salutation, opt => opt.MapFrom(src => src.Data.Salutation)).
-                        ForPath(dest => dest.FirstName, opt => opt.MapFrom(src => src.Data.FirstName)).
-                        ForPath(dest => dest.LastName, opt => opt.MapFrom(src => src.Data.LastName)).
-                        ForPath(dest => dest.DisplayName, opt => opt.MapFrom(src => src.Data.DisplayName)).
-                        ForPath(dest => dest.BirthDate, opt => opt.MapFrom(src => src.Data.BirthDate.ToDateTime())).
-                        ForPath(dest => dest.CreationTimestamp, opt => opt.MapFrom(src => src.Data.CreationTimestamp.ToDateTime())).
-                        ForPath(dest => dest.LastChangeTimeStamp, opt => opt.MapFrom(src => src.Data.LastChangeTimeStamp.ToDateTime())).
-                        ForPath(dest => dest.NotifyHasBirthdaySoon, opt => opt.MapFrom(src => src.Data.NotifyHasBirthdaySoon)).
-                        ForPath(dest => dest.Email, opt => opt.MapFrom(src => src.Data.Email)).
-                        ForPath(dest => dest.Phonenumber, opt => opt.MapFrom(src => src.Data.Phonenumber));
+        CreateMap<CreateContactProtoResponse, CreateContactResponse>().
+        ForPath(dest => dest.Id, opt => opt.MapFrom(src => src.Data.Id)).
+        ForPath(dest => dest.Salutation, opt => opt.MapFrom(src => src.Data.Salutation)).
+        ForPath(dest => dest.FirstName, opt => opt.MapFrom(src => src.Data.FirstName)).
+        ForPath(dest => dest.LastName, opt => opt.MapFrom(src => src.Data.LastName)).
+        ForPath(dest => dest.DisplayName, opt => opt.MapFrom(src => src.Data.DisplayName)).
+        ForPath(dest => dest.BirthDate, opt => opt.MapFrom(src => src.Data.BirthDate.ToDateTime())).
+        ForPath(dest => dest.CreationTimestamp, opt => opt.MapFrom(src => src.Data.CreationTimestamp.ToDateTime())).
+        ForPath(dest => dest.LastChangeTimeStamp, opt => opt.MapFrom(src => src.Data.LastChangeTimeStamp.ToDateTime())).
+        ForPath(dest => dest.NotifyHasBirthdaySoon, opt => opt.MapFrom(src => src.Data.NotifyHasBirthdaySoon)).
+        ForPath(dest => dest.Email, opt => opt.MapFrom(src => src.Data.Email)).
+        ForPath(dest => dest.Phonenumber, opt => opt.MapFrom(src => src.Data.Phonenumber));
 
         CreateMap<UpdateContactProtoResponse, UpdateContactResponse>();
         CreateMap<DeleteContactProtoResponse, DeleteContactResponse>();
 
         CreateMap<CreateContactRequest, CreateContactProtoRequest>()
-            .ForMember(dest => dest.BirthDate, opt =>
-                opt.MapFrom(src => Timestamp.FromDateTime(src.BirthDate.ToUniversalTime()))).ReverseMap();
+        .ForMember(dest => dest.BirthDate, opt =>
+        opt.MapFrom(src => Timestamp.FromDateTime(src.BirthDate.ToUniversalTime()))).ReverseMap();
 
         CreateMap<UpdateContactRequest, UpdateContactProtoRequest>()
-            .ForMember(dest => dest.BirthDate, opt =>
-                opt.MapFrom(src => Timestamp.FromDateTime(src.BirthDate.ToUniversalTime())));
+        .ForMember(dest => dest.BirthDate, opt =>
+        opt.MapFrom(src => Timestamp.FromDateTime(src.BirthDate.ToUniversalTime())));
 
         CreateMap<DeleteContactRequest, DeleteContactProtoRequest>();
     }
